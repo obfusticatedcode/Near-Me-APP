7331bc51aa46d2855f373cdf1ccb186c9c5b2065
Chisomo Lungu <chisomo@gmail.com>
2018-08-04 23:39:03 +0100

    added an about page plus routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('NearMeApp', ['leaflet-directive']);
+var app = angular.module('NearMeApp', ['ngRoute', 'leaflet-directive']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'MainController',
+      templateUrl: '../views/main.html'
+    })
+    .when('/about', {
+      controller: 'AboutController',
+      templateUrl: '../views/about.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
